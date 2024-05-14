65549e3578a0c367c50b933c0951374bc5d9eeac
aexp <desenvolvedores@agoraentert.com.br>
2024-05-14 14:46:31 -0300

    feat: add PostgresCreateUserRepository

--- index.js
+++ index.js
@@ -4,11 +4,20 @@ import express from "express";
 import { PostgresHelper } from "./src/db/postgres/helper.js";
 
 const app = express();
+app.use(express.json());
 
-app.get("/", async (req, res) => {
+app.get("/api/users", async (req, res) => {
     const results = await PostgresHelper.query("SELECT * FROM users;");
 
     res.send(JSON.stringify(results));
 });
 
-app.listen(3000, () => console.log("listenning on port 3000"));
+app.post("/api/users", async (req, res) => {
+    console.log(req.body);
+
+    res.status(201).send("User created");
+});
+
+app.listen(process.env.PORT, () =>
+    console.log(`listenning on port ${process.env.PORT}`),
+);
