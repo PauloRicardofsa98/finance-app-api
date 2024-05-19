000598984451339334a43c9136961cd4e0f01a64
Paulo Ricardo <paulo.ricardofsa98@gmail.com>
2024-05-19 10:42:25 -0300

    feat: remove not used functions helpers

--- src/controllers/helpers/transaction.js
+++ src/controllers/helpers/transaction.js
@@ -1,33 +1,4 @@
-import validator from "validator";
-import { badRequest, notFound } from "./http.js";
-
-export const checkIfAmountIsValid = (amount) => {
-    if (typeof amount !== "number") {
-        return false;
-    }
-
-    return validator.isCurrency(amount.toFixed(2), {
-        digits_after_decimal: [2],
-        allow_decimal: true,
-        decimal_separator: ".",
-    });
-};
-
-export const checkIfTypeIsValid = (type) => {
-    return ["EARNING", "EXPENSE", "INVESTMENT"].includes(type);
-};
-
-export const invalidAmountResponse = () => {
-    return badRequest({
-        message: "the amount must be a valid currency",
-    });
-};
-
-export const invalidTypeResponse = () => {
-    return badRequest({
-        message: "The type must be EARNING, EXPENSE, INVESTMENT.",
-    });
-};
+import { notFound } from "./http.js";
 
 export const transactionNotFoundResponse = () =>
     notFound({ message: "Transaction not found." });
