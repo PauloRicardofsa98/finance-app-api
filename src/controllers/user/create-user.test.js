34d6ece69278294855e207fc7ba3f12170e41308
Paulo Ricardo <paulo.ricardofsa98@gmail.com>
2024-09-01 11:40:27 -0300

    test: ensure 400 is returned if first_name is not provided

--- src/controllers/user/create-user.test.js
+++ src/controllers/user/create-user.test.js
@@ -31,4 +31,26 @@ describe("Create user Controller", () => {
         expect(result.body).not.toBeNull();
         expect(result.body).not.toBeUndefined();
     });
+
+    it('should return 400 when "first_name" is not provided', async () => {
+        //arrange
+        const createUserUseCase = new CreateUserUseCaseStub();
+        const createUserController = new CreateUserController(
+            createUserUseCase,
+        );
+
+        const httpRequest = {
+            body: {
+                last_name: "Doe",
+                email: "test@test.com",
+                password: "123456",
+            },
+        };
+
+        //act
+        const result = await createUserController.execute(httpRequest);
+
+        //assert
+        expect(result.statusCode).toBe(400);
+    });
 });
