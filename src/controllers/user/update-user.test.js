d9a8f98af0391706b8e21f141805e7126c2b3287
Paulo Ricardo <paulo.ricardofsa98@hmail.com>
2025-01-16 12:49:46 -0300

    test: add UpdateUserController tests for successful user update

---
+++ src/controllers/user/update-user.test.js
@@ -0,0 +1,42 @@
+import { faker } from "@faker-js/faker";
+import { UpdateUserController } from "./update-user";
+
+describe("Update User Controller", () => {
+    class UpdateUserUseCaseStub {
+        async execute(user) {
+            return user;
+        }
+    }
+
+    const makeSut = () => {
+        const updateUserUseCase = new UpdateUserUseCaseStub();
+        const sut = new UpdateUserController(updateUserUseCase);
+
+        return { sut, updateUserUseCase };
+    };
+
+    const httpRequest = {
+        params: {
+            userId: faker.string.uuid(),
+        },
+        body: {
+            first_name: faker.person.firstName(),
+            last_name: faker.person.lastName(),
+            email: faker.internet.email(),
+            password: faker.internet.password({
+                length: 8,
+            }),
+        },
+    };
+
+    it("should return 200 with updated user", async () => {
+        //arrange
+        const { sut } = makeSut();
+
+        // act
+        const response = await sut.execute(httpRequest);
+
+        // assert
+        expect(response.statusCode).toBe(200);
+    });
+});
