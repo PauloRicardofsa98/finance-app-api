d60c79fe6c80a637f8de2fa4b6abc900ac9acd1c
Paulo Ricardo <paulo.ricardofsa98@hmail.com>
2025-01-16 15:40:40 -0300

    test: add unit tests for CreateTransactionController

---
+++ src/controllers/transaction/create-transaction.test.js
@@ -0,0 +1,38 @@
+import { CreateTransactionController } from "./create-transaction";
+import { faker } from "@faker-js/faker";
+
+describe("Create Transaction Controller", () => {
+    class CreateTransactionUseCaseStub {
+        async execute(transaction) {
+            transaction;
+        }
+    }
+
+    const makeSut = () => {
+        const createTransactionUseCase = new CreateTransactionUseCaseStub();
+        const sut = new CreateTransactionController(createTransactionUseCase);
+
+        return { createTransactionUseCase, sut };
+    };
+
+    const httpRequest = {
+        body: {
+            user_id: faker.string.uuid(),
+            name: faker.string.alphanumeric(10),
+            date: faker.date.anytime().toISOString(),
+            type: "EXPENSE",
+            amount: Number(faker.finance.amount()),
+        },
+    };
+
+    it("should return 201 when creating a transaction successfully", async () => {
+        //arrange
+        const { sut } = makeSut();
+
+        //act
+        const result = await sut.execute(httpRequest);
+
+        //assert
+        expect(result.statusCode).toBe(201);
+    });
+});
