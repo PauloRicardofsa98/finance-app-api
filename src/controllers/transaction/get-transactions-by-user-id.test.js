895ce6d790850dc8b15d0d6e5e6169707634b457
Paulo Ricardo <paulo.ricardofsa98@hmail.com>
2025-01-16 16:10:59 -0300

    test: add validation cases for missing and invalid userId in GetTransactionsByUserIdController

--- src/controllers/transaction/get-transactions-by-user-id.test.js
+++ src/controllers/transaction/get-transactions-by-user-id.test.js
@@ -29,20 +29,48 @@ describe("Get Transactions By User Id Controller", () => {
         };
     };
 
-    const httpRequest = {
-        query: {
-            userId: faker.string.uuid(),
-        },
-    };
-
     it("should return 200 when finding transaction by user id successfully", async () => {
         //arrange
         const { sut } = makeSut();
 
         //act
-        const response = await sut.execute(httpRequest);
+        const response = await sut.execute({
+            query: {
+                userId: faker.string.uuid(),
+            },
+        });
 
         //assert
         expect(response.statusCode).toBe(200);
+    });
+
+    it("should return 400 when userId is not provided", async () => {
+        //arrange
+        const { sut } = makeSut();
+        const httpRequestWithoutUserId = {
+            query: {},
+        };
+
+        //act
+        const response = await sut.execute(httpRequestWithoutUserId);
+
+        //assert
+        expect(response.statusCode).toBe(400);
+    });
+
+    it("should return 400 when userId is invalid", async () => {
+        //arrange
+        const { sut } = makeSut();
+        const httpRequestWithInvalidUserId = {
+            query: {
+                userId: "invalid_id",
+            },
+        };
+
+        //act
+        const response = await sut.execute(httpRequestWithInvalidUserId);
+
+        //assert
+        expect(response.statusCode).toBe(400);
     });
 });
