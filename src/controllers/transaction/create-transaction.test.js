d16d5e33d6b0bb32e557c6ec059b3ce116ba3138
Paulo Ricardo <paulo.ricardofsa98@hmail.com>
2025-01-16 15:43:07 -0300

    test: add validation case for missing name in CreateTransactionController

--- src/controllers/transaction/create-transaction.test.js
+++ src/controllers/transaction/create-transaction.test.js
@@ -51,4 +51,19 @@ describe("Create Transaction Controller", () => {
         //assert
         expect(result.statusCode).toBe(400);
     });
+    it("should return 400 when missing name", async () => {
+        //arrange
+        const { sut } = makeSut();
+
+        //act
+        const result = await sut.execute({
+            body: {
+                ...httpRequest,
+                name: undefined,
+            },
+        });
+
+        //assert
+        expect(result.statusCode).toBe(400);
+    });
 });
