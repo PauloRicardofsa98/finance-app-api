773cd5ab0e9f7c49dcb9db471d64f968fc9b2176
Paulo Ricardo <paulo.ricardofsa98@hmail.com>
2025-01-17 10:55:53 -0300

    feat: add test for DeleteUserUseCase to verify successful user deletion

---
+++ src/use-cases/user/delete-user.test.js
@@ -0,0 +1,38 @@
+import { DeleteUserUseCase } from "./delete-user";
+import { faker } from "@faker-js/faker";
+
+describe("DeleteUserUseCase", () => {
+    const user = {
+        first_name: faker.person.firstName(),
+        last_name: faker.person.lastName(),
+        email: faker.internet.email(),
+        password: faker.internet.password(),
+    };
+
+    class DeleteUserRepositoryStub {
+        async execute() {
+            return user;
+        }
+    }
+
+    const makeSut = () => {
+        const deleteUserRepository = new DeleteUserRepositoryStub();
+        const sut = new DeleteUserUseCase(deleteUserRepository);
+        return {
+            sut,
+            deleteUserRepository,
+        };
+    };
+
+    it("should successfully delete a user", async () => {
+        // Arrange
+        const { sut } = makeSut();
+
+        // Act
+        const response = await sut.execute(faker.string.uuid());
+
+        // Assert
+        expect(response).toBeTruthy();
+        expect(response).toEqual(user);
+    });
+});
