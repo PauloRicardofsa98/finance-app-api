ad8630b4ab288702127a1729aa1f19d45bf58883
Paulo Ricardo <paulo.ricardofsa98@hmail.com>
2025-01-17 10:51:39 -0300

    feat: add test to CreateUserUseCase for handling errors from PasswordHasherAdapter

--- src/use-cases/user/create-user.test.js
+++ src/use-cases/user/create-user.test.js
@@ -139,4 +139,18 @@ describe("Create User Use Case", () => {
         // Assert
         await expect(promise).rejects.toThrow();
     });
+
+    it("should throw if PasswordHasherAdapter throws", async () => {
+        // Arrange
+        const { sut, passwordHasherAdapter } = makeSut();
+        jest.spyOn(passwordHasherAdapter, "execute").mockRejectedValueOnce(
+            new Error()
+        );
+
+        // Act
+        const promise = sut.execute(user);
+
+        // Assert
+        await expect(promise).rejects.toThrow();
+    });
 });
