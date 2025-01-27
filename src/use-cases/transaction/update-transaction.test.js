e8a6af22dedc6d9d89e76fd2683c1ec24daa70b3
Paulo Ricardo <paulo.ricardofsa98@hmail.com>
2025-01-27 11:47:21 -0300

    test: ensure UpdateTransactionUseCase calls updateTransactionRepository with correct params

--- src/use-cases/transaction/update-transaction.test.js
+++ src/use-cases/transaction/update-transaction.test.js
@@ -1,4 +1,5 @@
 import { faker } from "@faker-js/faker";
+import { UpdateTransactionUseCase } from "./update-transaction";
 
 describe("UpdateTransactionUseCase", () => {
     const transaction = {
@@ -9,26 +10,43 @@ describe("UpdateTransactionUseCase", () => {
         type: "EXPENSE",
         amount: Number(faker.finance.amount()),
     };
-    class UpdateTransactionUseCase {
+    class UpdateTransactionRepositoryStub {
         async execute(transactionId) {
             return { id: transactionId, ...transaction };
         }
     }
 
     const makeSut = () => {
-        const updateTransactionRepositoryStub = new UpdateTransactionUseCase();
-        const sut = new UpdateTransactionUseCase(
-            updateTransactionRepositoryStub
-        );
-        return { sut, updateTransactionRepositoryStub };
+        const updateTransactionRepository =
+            new UpdateTransactionRepositoryStub();
+        const sut = new UpdateTransactionUseCase(updateTransactionRepository);
+        return { sut, updateTransactionRepository };
     };
 
     it("should update a transaction", async () => {
         // Arrange
         const { sut } = makeSut();
         // Act
-        const response = await sut.execute(transaction.id, { amount: 100 });
+        const response = await sut.execute(transaction.id, {
+            amount: faker.finance.amount(),
+        });
         // Assert
         expect(response).toEqual(transaction);
+    });
+
+    it("should call updateTransactionRepository with correct params", async () => {
+        // Arrange
+        const { sut, updateTransactionRepository } = makeSut();
+        const updateTransactionRepositorySpy = jest.spyOn(
+            updateTransactionRepository,
+            "execute"
+        );
+        // Act
+        await sut.execute(transaction.id, { amount: transaction.amount });
+        // Assert
+        expect(updateTransactionRepositorySpy).toHaveBeenCalledWith(
+            transaction.id,
+            { amount: transaction.amount }
+        );
     });
 });
