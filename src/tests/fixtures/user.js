1ec922e6d7b0e0f4fdcc226509fe66ba3b72261a
Paulo Ricardo <paulo.ricardofsa98@hmail.com>
2025-01-27 12:07:54 -0300

    test: add userBalance fixture for enhanced testing scenarios

--- src/tests/fixtures/user.js
+++ src/tests/fixtures/user.js
@@ -7,3 +7,10 @@ export const user = {
     email: faker.internet.email(),
     password: faker.internet.password({ length: 7 }),
 };
+
+export const userBalance = {
+    earning: faker.number.int(),
+    expenses: faker.number.int(),
+    investments: faker.number.int(),
+    balance: faker.number.int(),
+};
