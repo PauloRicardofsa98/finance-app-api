f4a8d747274519b826f432de49e24e2b76f48a5d
Paulo Ricardo <paulo.ricardofsa98@hmail.com>
2025-01-29 09:55:28 -0300

    fix: correct update parameters in PostgresUpdateTransactionRepository

--- src/repositories/postgres/transaction/update-transaction.js
+++ src/repositories/postgres/transaction/update-transaction.js
@@ -2,7 +2,8 @@ import { prisma } from "../../../../prisma/prisma.js";
 export class PostgresUpdateTransactionRepository {
     async execute(transactionId, updateTransactionsParams) {
         const transaction = await prisma.transaction.update({
-            where: { id: transactionId, data: updateTransactionsParams },
+            where: { id: transactionId },
+            data: updateTransactionsParams,
         });
 
         return transaction;
