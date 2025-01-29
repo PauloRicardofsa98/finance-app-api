47e32a2d7595309ec5e72feff0cce5e5ec0d4934
Paulo Ricardo <paulo.ricardofsa98@hmail.com>
2025-01-29 09:02:32 -0300

    test: ensure PostgresCreateTransactionRepository creates a transaction on db

---
+++ src/repositories/postgres/transaction/create-transaction.test.js
@@ -0,0 +1,22 @@
+import { prisma } from "../../../../prisma/prisma";
+import { PostgresCreateTransactionRepository } from "./create-transaction";
+import { user as fakerUser, transaction } from "../../../tests";
+import dayjs from "dayjs";
+
+describe("CreateTransactionRepository", () => {
+    it("should create a transaction on db", async () => {
+        const user = await prisma.user.create({ data: fakerUser });
+        // Arrange
+        const sut = new PostgresCreateTransactionRepository();
+
+        // Act
+        const result = await sut.execute({ ...transaction, user_id: user.id });
+
+        // Assert
+        expect(result.name).toBe(transaction.name);
+        expect(result.value).toBe(transaction.value);
+        expect(result.user_id).toBe(user.id);
+        expect(Number(result.type)).toBe(Number(transaction.type));
+        expect(dayjs(result.date).day).toBe(dayjs(transaction.date).day);
+    });
+});
