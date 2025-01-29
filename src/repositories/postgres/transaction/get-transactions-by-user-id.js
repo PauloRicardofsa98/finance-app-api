cedd923f33697dc81390254d1cf7e3d9d364f5fc
Paulo Ricardo <paulo.ricardofsa98@hmail.com>
2025-01-29 09:41:11 -0300

    fix: update PostgresGetTransactionsByUserIdRepository to fetch multiple transactions

--- src/repositories/postgres/transaction/get-transactions-by-user-id.js
+++ src/repositories/postgres/transaction/get-transactions-by-user-id.js
@@ -1,9 +1,9 @@
 import { prisma } from "../../../../prisma/prisma";
 
 export class PostgresGetTransactionsByUserIdRepository {
-    async execute(user_id) {
-        const transactions = await prisma.transaction.findUnique({
-            where: { user_id: user_id },
+    async execute(userId) {
+        const transactions = await prisma.transaction.findMany({
+            where: { user_id: userId },
         });
 
         return transactions;
