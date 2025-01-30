7c9413c48366f0cceb544a7b82c051dc3c573839
Paulo Ricardo <paulo.ricardofsa98@hmail.com>
2025-01-30 17:39:53 -0300

    test: ensure PostgresDeleteUserRepository throw UserNotFoundError if user does not exist

--- src/repositories/postgres/user/delete-user.test.js
+++ src/repositories/postgres/user/delete-user.test.js
@@ -1,6 +1,7 @@
 import { prisma } from "../../../../prisma/prisma";
 import { PostgresDeleteUserRepository } from "./delete-user";
 import { user } from "../../../tests";
+import { UserNotFoundError } from "../../../errors";
 
 describe("DeleteUserRepository", () => {
     it("should delete user on db", async () => {
@@ -32,15 +33,29 @@ describe("DeleteUserRepository", () => {
         });
     });
 
-    // it("should throw if prisma throws", async () => {
-    //     // Arrange
-    //     const sut = new PostgresDeleteUserRepository();
-    //     jest.spyOn(prisma.user, "delete").mockRejectedValueOnce(new Error());
+    it("should throw if prisma throws", async () => {
+        // Arrange
+        const sut = new PostgresDeleteUserRepository();
+        jest.spyOn(prisma.user, "delete").mockRejectedValueOnce(new Error());
 
-    //     // Act
-    //     const promise = sut.execute(user.id);
+        // Act
+        const promise = sut.execute(user.id);
 
-    //     // Assert
-    //     await expect(promise).rejects.toThrow();
-    // });
+        // Assert
+        await expect(promise).rejects.toThrow();
+    });
+
+    it("should throw UserNotFoundError if user does not exist", async () => {
+        // Arrange
+        const sut = new PostgresDeleteUserRepository();
+        jest.spyOn(prisma.user, "delete").mockRejectedValueOnce(
+            new UserNotFoundError()
+        );
+
+        // Act
+        const promise = sut.execute(user.id);
+
+        // Assert
+        await expect(promise).rejects.toThrow(new UserNotFoundError());
+    });
 });
