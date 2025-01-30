1ce936c4f9b37af50cd2bfe2532b9b3e61e201c3
Paulo Ricardo <paulo.ricardofsa98@hmail.com>
2025-01-30 17:38:55 -0300

    feat: enhance PostgresDeleteUserRepository to throw UserNotFoundError for missing users

--- src/repositories/postgres/user/delete-user.js
+++ src/repositories/postgres/user/delete-user.js
@@ -1,11 +1,18 @@
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 import { prisma } from "../../../../prisma/prisma.js";
+import { UserNotFoundError } from "../../../errors/user.js";
 
 export class PostgresDeleteUserRepository {
     async execute(userId) {
         try {
             return await prisma.user.delete({ where: { id: userId } });
-        } catch {
-            return null;
+        } catch (error) {
+            if (error instanceof PrismaClientKnownRequestError) {
+                if (error.code === "P2025") {
+                    throw new UserNotFoundError(userId);
+                }
+            }
+            throw error;
         }
     }
 }
