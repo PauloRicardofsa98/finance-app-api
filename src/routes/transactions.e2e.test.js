0b4c2ac9943ddcc4bde39348066fafc9557a8f88
Paulo Ricardo <paulo.ricardofsa98@hmail.com>
2025-01-31 11:52:28 -0300

    test: ensure PATCH /api/transactions/:transactionId should return 200 when transaction is updated

--- src/routes/transactions.e2e.test.js
+++ src/routes/transactions.e2e.test.js
@@ -48,4 +48,34 @@ describe("Transaction Routes E2E Tests", () => {
         expect(response.status).toBe(200);
         expect(response.body[0]).toEqual(createdTransaction);
     });
+
+    it("PATCH /api/transactions/:transactionId should return 200 when transaction is updated", async () => {
+        const { body: createdUser } = await request(app)
+            .post("/api/users")
+            .send({
+                ...user,
+                id: undefined,
+            });
+
+        const { body: createdTransaction } = await request(app)
+            .post("/api/transactions")
+            .send({
+                ...transaction,
+                id: undefined,
+                user_id: createdUser.id,
+            });
+
+        const updateTransactionParams = {
+            amount: 100,
+        };
+
+        const response = await request(app)
+            .patch(`/api/transactions/${createdTransaction.id}`)
+            .send(updateTransactionParams);
+
+        expect(response.status).toBe(200);
+        expect(response.body.amount).toBe(
+            String(updateTransactionParams.amount)
+        );
+    });
 });
