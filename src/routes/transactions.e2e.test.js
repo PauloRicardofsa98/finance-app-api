fcadf2752b214d38a42e26f5a45f1d04dad5c7e7
Paulo Ricardo <paulo.ricardofsa98@hmail.com>
2025-01-31 11:51:16 -0300

    test: ensure GET /api/transactions?userId should return 200 when transaction is found

--- src/routes/transactions.e2e.test.js
+++ src/routes/transactions.e2e.test.js
@@ -24,4 +24,28 @@ describe("Transaction Routes E2E Tests", () => {
         expect(response.body.amount).toBe(String(transaction.amount));
         expect(response.body.type).toBe(transaction.type);
     });
+
+    it("GET /api/transactions?userId should return 200 when transaction is found", async () => {
+        const { body: createdUser } = await request(app)
+            .post("/api/users")
+            .send({
+                ...user,
+                id: undefined,
+            });
+
+        const { body: createdTransaction } = await request(app)
+            .post("/api/transactions")
+            .send({
+                ...transaction,
+                id: undefined,
+                user_id: createdUser.id,
+            });
+
+        const response = await request(app).get(
+            `/api/transactions?userId=${createdUser.id}`
+        );
+
+        expect(response.status).toBe(200);
+        expect(response.body[0]).toEqual(createdTransaction);
+    });
 });
