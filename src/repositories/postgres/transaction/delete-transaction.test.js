0f09cc7592eca62b93a26688c831b2d50be8e207
Paulo Ricardo <paulo.ricardofsa98@hmail.com>
2025-01-31 12:18:40 -0300

    refactor: remove unnecessary date assertions in DeleteTransactionRepository tests

--- src/repositories/postgres/transaction/delete-transaction.test.js
+++ src/repositories/postgres/transaction/delete-transaction.test.js
@@ -1,7 +1,6 @@
 import { prisma } from "../../../../prisma/prisma";
 import { PostgresDeleteTransactionRepository } from "./delete-transaction";
 import { user, transaction } from "../../../tests";
-import dayjs from "dayjs";
 import { TransactionNotFoundError } from "../../../errors";
 import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 
@@ -24,13 +23,6 @@ describe("DeleteTransactionRepository", () => {
         expect(result.type).toBe(transaction.type);
         expect(result.user_id).toBe(user.id);
         expect(String(result.amount)).toBe(String(transaction.amount));
-        expect(dayjs(result.date).daysInMonth()).toBe(
-            dayjs(transaction.date).daysInMonth()
-        );
-        expect(dayjs(result.date).month()).toBe(
-            dayjs(transaction.date).month()
-        );
-        expect(dayjs(result.date).year()).toBe(dayjs(transaction.date).year());
     });
 
     it("should call Prisma with correct params", async () => {
