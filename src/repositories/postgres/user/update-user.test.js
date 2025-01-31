9d537b3f67c643b6340866c0da9728a4a22d9632
Paulo Ricardo <paulo.ricardofsa98@hmail.com>
2025-01-31 11:24:34 -0300

    test: add UserNotFoundError handling in update user repository tests

--- src/repositories/postgres/user/update-user.test.js
+++ src/repositories/postgres/user/update-user.test.js
@@ -1,6 +1,9 @@
 import { prisma } from "../../../../prisma/prisma";
 import { PostgresUpdateUserRepository } from "./update-user";
 import { user as fakeUser } from "../../../tests";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
+import { UserNotFoundError } from "../../../errors";
+import { faker } from "@faker-js/faker";
 
 describe("UpdateUserRepository", () => {
     it("should update a user", async () => {
@@ -43,5 +46,24 @@ describe("UpdateUserRepository", () => {
 
         // Assert
         await expect(promise).rejects.toThrow();
+    });
+
+    it("should throw UserNotFoundError if user does not exist to update", async () => {
+        // Arrange
+        const sut = new PostgresUpdateUserRepository();
+        jest.spyOn(prisma.user, "update").mockRejectedValueOnce(
+            new PrismaClientKnownRequestError("", {
+                code: "P2025",
+            })
+        );
+
+        const userId = faker.string.uuid();
+        // Act
+        const promise = sut.execute(userId, {
+            first_name: "new name",
+        });
+
+        // Assert
+        await expect(promise).rejects.toThrow(new UserNotFoundError(userId));
     });
 });
