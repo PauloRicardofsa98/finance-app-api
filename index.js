eef2dd15848156e63e3aaf37f7cbb6055679932e
Paulo Ricardo <paulo.ricardofsa98@hmail.com>
2025-02-03 15:47:47 -0300

    docs: add post /api/user to swagger docs

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import "dotenv/config.js";
-import { app } from "./src/app";
+import { app } from "./src/app.js";
 
 app.listen(process.env.PORT, () =>
     console.log(`listenning on port ${process.env.PORT}`)
--- src/app.js
+++ src/app.js
@@ -1,8 +1,20 @@
 import express from "express";
 import { transactionRouter, userRouter } from "./routes/index.js";
+import swaggerUi from "swagger-ui-express";
+import fs from "fs";
+import path from "path";
 
 export const app = express();
 app.use(express.json());
 
 app.use("/api/users", userRouter);
 app.use("/api/transactions", transactionRouter);
+
+const swaggerDocument = JSON.parse(
+    fs.readFileSync(
+        path.join(import.meta.dirname, "../docs/swagger.json"),
+        "utf-8"
+    )
+);
+
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
