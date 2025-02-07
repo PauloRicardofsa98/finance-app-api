ce025c05c758fb588cb975afd0b3315ad043bb26
Paulo Ricardo <paulo.ricardofsa98@hmail.com>
2025-02-07 11:24:53 -0300

    feat: add LoginUserController

---
+++ src/controllers/user/login-user.js
@@ -0,0 +1,41 @@
+import { ZodError } from "zod";
+import { InvalidPasswordError, UserNotFoundError } from "../../errors";
+import { loginSchema } from "../../schemas/user";
+import {
+    badRequest,
+    ok,
+    serverError,
+    unauthorized,
+    userNotFoundResponse,
+} from "../helpers";
+
+export class LoginUserController {
+    constructor(loginUserUseCase) {
+        this.loginUserUseCase = loginUserUseCase;
+    }
+    async execute(request) {
+        try {
+            const { email, password } = request.body;
+
+            await loginSchema.parseAsync({ email, password });
+
+            const user = await this.loginUserUseCase.execute({
+                email,
+                password,
+            });
+            return ok(user);
+        } catch (error) {
+            if (error instanceof UserNotFoundError) {
+                return userNotFoundResponse();
+            }
+            if (error instanceof InvalidPasswordError) {
+                return unauthorized();
+            }
+            if (error instanceof ZodError) {
+                badRequest(error.errors);
+            }
+
+            return serverError();
+        }
+    }
+}
--- src/schemas/user.js
+++ src/schemas/user.js
@@ -39,3 +39,23 @@ export const createUserSchema = z.object({
 export const updateUserSchema = createUserSchema
     .partial()
     .strict({ message: "Some provided field is not allowed!" });
+
+export const loginSchema = z.object({
+    email: z
+        .string()
+        .email({
+            message: "Please provide a valid e-mail.",
+        })
+        .trim()
+        .min(1, {
+            message: "E-mail is required.",
+        }),
+    password: z
+        .string({
+            required_error: "Password is required",
+        })
+        .trim()
+        .min(6, {
+            message: "Password must have at least 6 characters.",
+        }),
+});
