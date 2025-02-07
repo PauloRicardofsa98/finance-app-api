30b383a566487b04bdf12112f41e22a3dc0bfbc7
Paulo Ricardo <paulo.ricardofsa98@hmail.com>
2025-02-07 11:04:54 -0300

    feat: add LoginUserUseCase

---
+++ src/use-cases/user/login-user.js
@@ -0,0 +1,32 @@
+import { InvalidPasswordError, UserNotFoundError } from "../../errors";
+
+export class LoginUserUseCase {
+    constructor(getUserByEmailRepository, passwordComparator, tokensGenerator) {
+        this.getUserByEmailRepository = getUserByEmailRepository;
+        this.passwordComparator = passwordComparator;
+        this.tokensGenerator = tokensGenerator;
+    }
+
+    async execute(email, password) {
+        const user = await this.getUserByEmailRepository.execute(email);
+        if (!user) {
+            throw new UserNotFoundError();
+        }
+
+        const isPasswordValid = await this.passwordComparator.execute(
+            password,
+            user.password,
+        );
+
+        if (!isPasswordValid) {
+            throw new InvalidPasswordError();
+        }
+
+        const tokens = this.tokensGenerator.execute(user.id);
+
+        return {
+            ...user,
+            tokens,
+        };
+    }
+}
