0ca882a0bc72405ff01031a37268ed5f28f59d7d
Paulo Ricardo <paulo.ricardofsa98@hmail.com>
2025-02-10 15:30:09 -0300

    feat: extend transaction retrieval to support date range parameters

--- src/use-cases/transaction/get-transactions-by-user-id.js
+++ src/use-cases/transaction/get-transactions-by-user-id.js
@@ -7,14 +7,18 @@ export class GetTransactionsByUserIdUseCase {
         this.getUserByIdRepository = getUserByIdRepository;
     }
 
-    async execute(userId) {
+    async execute(userId, from, to) {
         const user = await this.getUserByIdRepository.execute(userId);
         if (!user) {
             throw new UserNotFoundError(userId);
         }
 
         const transactions =
-            await this.getTransactionsByUserIdRepository.execute(userId);
+            await this.getTransactionsByUserIdRepository.execute(
+                userId,
+                from,
+                to,
+            );
 
         return transactions;
     }
