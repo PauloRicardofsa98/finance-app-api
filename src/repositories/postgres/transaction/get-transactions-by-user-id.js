abf78cc371a2552a8855d19e375eb83772320b68
Paulo Ricardo <paulo.ricardofsa98@hmail.com>
2025-02-10 15:40:26 -0300

    feat: enhance transaction retrieval by adding date range filtering

--- src/repositories/postgres/transaction/get-transactions-by-user-id.js
+++ src/repositories/postgres/transaction/get-transactions-by-user-id.js
@@ -1,9 +1,15 @@
 import { prisma } from "../../../../prisma/prisma.js";
 
 export class PostgresGetTransactionsByUserIdRepository {
-    async execute(userId) {
+    async execute(userId, from, to) {
         const transactions = await prisma.transaction.findMany({
-            where: { userId: userId },
+            where: {
+                userId: userId,
+                date: {
+                    gte: new Date(from),
+                    lte: new Date(to),
+                },
+            },
         });
 
         return transactions;
